bcd24b9d690a77960c21903d495ec42dfcda82cd
pstar <pst@ngc.dk>
2022-06-24 09:33:18 +0200

    added initial code for neural networks and all vs all genomes

--- tutorial.py
+++ tutorial.py
@@ -39,12 +39,98 @@ class PongGame:
 
         pygame.quit()
 
+    # have the genomes play the game
+    def train_ai(self, genome1, genome2, config):
+
+        # create neural network for genomes, pass inputs, get outputs
+        net1 = neat.nn.FeedForwardNetwork.create(genome1, config)
+        net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
+
+        # start and quit the game
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+
+            # get outputs from the nerual networks
+            # numeric values for the neural networks
+            output1 = net1.activate(
+                (self.left_paddle.y, self.ball.y, abs(self.left_paddle.x - self.ball.x)))
+
+            output2 = net2.activate(
+                (self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
+            print(output1, output2)
+
+            # get the highest number from output 1
+            # 0 stay still
+            # 1 move up
+            # 2 move down
+            decision1 = output1.index(max(output1))
+            decision2 = output2.index(max(output2))
+
+            # based on the index, make a move
+            if decision1 == 0:
+                pass
+            elif decision1 == 1:
+                self.game.move_paddle(left=True, up=True)
+            else:
+                self.game.move_paddle(left=True, up=False)
+
+            if decision2 == 0:
+                pass
+            elif decision2 == 1:
+                self.game.move_paddle(left=False, up=True)
+            else:
+                self.game.move_paddle(left=False, up=False)
+
+            # get game info
+            game_info = self.game.loop()
+
+            # draw and update
+            self.game.draw()
+            pygame.display.update()
+
+            # stop after the first miss
+            # calulates fitness
+            if game_info.left_score >= 1 or game_info.right_score >= 1:
+                self.calulcate_fitness(genome1, genome2, game_info)
+                break
+
+    def calulcate_fitness(self, genome1, genome2, game_info):
+        pass
+
 # how good? dependent on opponent
 # each ai vs every other ai
 
 
 def eval_genomes(genomes, config):
-    pass
+    width, height = 700, 500
+    window = pygame.display.set_mode((width, height))
+
+    # genomes - list of tuples, genomeID and genome object
+    # if every genome vs other genome then enumerate
+    for i, (genome_id1, genome1) in enumerate(genomes):
+
+        # if the lest index, break
+        if i == len(genomes) - 1:
+            break
+
+        # set default fitness for a genome
+        genome1.fitness = 0
+
+        # riun each genome 1 vs other genomes exactly 1
+        # fitness is the SUM of allgames
+        for genome_id2, genome2 in genomes[i+1:]:
+
+            # check if genome2 has a fitness value, not to overwrite it
+            genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+
+            # initiate the game
+            game = PongGame(window, width, height)
+
+            # train the ai
+            game.train_ai(genome1, genome2, config)
 
 
 def run_neat(config):
@@ -67,7 +153,10 @@ def run_neat(config):
     p.run(eval_genomes, 50)
 
 
+# main loop
 if __name__ == "__main__":
+
+    # read config from local dir
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config.txt')
 
